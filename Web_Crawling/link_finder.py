45cad0db97d074a471bcf8d4bad5144913b84956
Sanchit Bansal <sanchitbansal.06@gmail.com>
2018-12-29 20:11:17 +0530

    Updated

---
+++ Web_Crawling/link_finder.py
@@ -0,0 +1,24 @@
+from HTMLParser import HTMLParser
+from urlparse import urlparse
+
+class LinkFinder(HTMLParser):
+
+	def __init__(self, base_url, page_url):
+		self.base_url = base_url
+		self.page_url = page_url
+		self.links = set()
+
+
+	def error(self, message):
+		pass
+
+	def handle_starttag(self, tag, attrs):
+		if tag == 'a':
+			for (attribute, value) in attrs:
+				if attribute == 'href':
+					url = parse.urljoin(self.base_url, value)
+					self.links.add(url)
+
+
+	def page_links(self):
+		return self.links
